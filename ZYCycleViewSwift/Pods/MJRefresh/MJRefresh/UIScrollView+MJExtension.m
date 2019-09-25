6d83d52b3cd0b26b144c7d95f255947e62ea9fae
zhouyu <zhouyu@haodf.com>
2019-09-25 11:08:22 +0800

    升级兼容Xocde11和Swift 5

--- ZYCycleViewSwift/Pods/MJRefresh/MJRefresh/UIScrollView+MJExtension.m
+++ ZYCycleViewSwift/Pods/MJRefresh/MJRefresh/UIScrollView+MJExtension.m
@@ -10,17 +10,25 @@
 #import "UIScrollView+MJExtension.h"
 #import <objc/runtime.h>
 
-#define SYSTEM_VERSION_GREATER_NOT_LESS_THAN(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
-
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunguarded-availability-new"
 
 @implementation UIScrollView (MJExtension)
 
+static BOOL respondsToAdjustedContentInset_;
+
++ (void)initialize
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        respondsToAdjustedContentInset_ = [self instancesRespondToSelector:@selector(adjustedContentInset)];
+    });
+}
+
 - (UIEdgeInsets)mj_inset
 {
 #ifdef __IPHONE_11_0
-    if (SYSTEM_VERSION_GREATER_NOT_LESS_THAN(@"11.0")) {
+    if (respondsToAdjustedContentInset_) {
         return self.adjustedContentInset;
     }
 #endif
@@ -32,7 +40,7 @@
     UIEdgeInsets inset = self.contentInset;
     inset.top = mj_insetT;
 #ifdef __IPHONE_11_0
-    if (SYSTEM_VERSION_GREATER_NOT_LESS_THAN(@"11.0")) {
+    if (respondsToAdjustedContentInset_) {
         inset.top -= (self.adjustedContentInset.top - self.contentInset.top);
     }
 #endif
@@ -49,7 +57,7 @@
     UIEdgeInsets inset = self.contentInset;
     inset.bottom = mj_insetB;
 #ifdef __IPHONE_11_0
-    if (SYSTEM_VERSION_GREATER_NOT_LESS_THAN(@"11.0")) {
+    if (respondsToAdjustedContentInset_) {
         inset.bottom -= (self.adjustedContentInset.bottom - self.contentInset.bottom);
     }
 #endif
@@ -66,7 +74,7 @@
     UIEdgeInsets inset = self.contentInset;
     inset.left = mj_insetL;
 #ifdef __IPHONE_11_0
-    if (SYSTEM_VERSION_GREATER_NOT_LESS_THAN(@"11.0")) {
+    if (respondsToAdjustedContentInset_) {
         inset.left -= (self.adjustedContentInset.left - self.contentInset.left);
     }
 #endif
@@ -75,7 +83,7 @@
 
 - (CGFloat)mj_insetL
 {
-    return self.contentInset.left;
+    return self.mj_inset.left;
 }
 
 - (void)setMj_insetR:(CGFloat)mj_insetR
@@ -83,7 +91,7 @@
     UIEdgeInsets inset = self.contentInset;
     inset.right = mj_insetR;
 #ifdef __IPHONE_11_0
-    if (SYSTEM_VERSION_GREATER_NOT_LESS_THAN(@"11.0")) {
+    if (respondsToAdjustedContentInset_) {
         inset.right -= (self.adjustedContentInset.right - self.contentInset.right);
     }
 #endif
@@ -92,7 +100,7 @@
 
 - (CGFloat)mj_insetR
 {
-    return self.contentInset.right;
+    return self.mj_inset.right;
 }
 
 - (void)setMj_offsetX:(CGFloat)mj_offsetX
